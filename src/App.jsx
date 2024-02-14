2fbe6ba98906c0b8fd0cb98ade30dc38c4cdde65
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-02-14 23:50:29 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+
+function GrandChildComponent({parentData}) {
+  console.log(`From Grand Child Component, ${parentData}`)
+  return(
+    <div>
+      <h3>Grand Child Component</h3>
+    </div>
+  )
+}
+
+
+function ChildComponent({parentData}) {
+  console.log(`From Child Component, ${parentData}`)
+  return(
+    <div>
+      <h2>Child Component</h2>
+      <GrandChildComponent parentData={parentData}/>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(()=>{
-    document.title = `Count: ${count}`
-  },[count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  }
-console.log(count);
-
+  const parentData = 'Hello from Parent'
+ console.log(`From Parent Component, ${parentData}`)
   return (
     <div>
-      <h1>Document Title Change</h1>
-      <button onClick={handleClick}>Change Count</button>
+      <h1>Parent Component</h1>
+      <ChildComponent parentData={parentData}/>
     </div>
   )
 }
