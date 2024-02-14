37c0312cbd8ef3f6b96d5eb9a1d15e72e418ae4b
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-02-15 00:15:26 +0530

    passing data from child to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+// passing props from child to parent component
+import React, { useState } from 'react';
 
+function ChildComponent({onMessage}) {
 
-function GrandChildComponent({parentData}) {
-  console.log(`From Grand Child Component, ${parentData}`)
+  const message = 'Hello from Child'
   return(
-    <div>
-      <h3>Grand Child Component</h3>
-    </div>
-  )
-}
-
-
-function ChildComponent({parentData}) {
-  console.log(`From Child Component, ${parentData}`)
-  return(
-    <div>
-      <h2>Child Component</h2>
-      <GrandChildComponent parentData={parentData}/>
-    </div>
+    <>
+    <h2>Child Component</h2>
+    <button onClick={() => onMessage(message)}>Send Message to the Parent</button>
+    </>
   )
 }
 
 function App() {
 
-  const parentData = 'Hello from Parent'
- console.log(`From Parent Component, ${parentData}`)
+  const [message, setMessage] = useState('');
+
+  const handleMessage = (data) =>{
+    setMessage(data)
+  }
+
   return (
     <div>
       <h1>Parent Component</h1>
-      <ChildComponent parentData={parentData}/>
+      <p>Message from the Child Component: {message}</p>
+      <ChildComponent onMessage={handleMessage}/>
     </div>
   )
 }
