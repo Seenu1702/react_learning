adb1f3282ce794127e05c7a9c67248e1f7f3aad0
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-02-16 15:45:26 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,57 @@
-/* eslint-disable react-refresh/only-export-components */
-import React, { createContext, useState } from 'react';
-import ChildComponent from './components/ChildComponent';
-// to avoid props drilling, we can use useContext() and creatContext()...
-// create a context
-const MessageContext = createContext();
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import Note from './components/Note';
 
-function App() {
-  
-  const [message, setMessage] = useState('Hello from App');
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+
+  // console.log(notes);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    // console.log('button clicked');
+    console.log(newNote);
+
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    setNotes(notes.concat(noteObject)); 
+    setNewNote('');
+    
+  }
+
+  const handleNoteChange = (event) => {
+    // console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
+
   return (
     <div>
-      <h1>Parent Component</h1>
-      <MessageContext.Provider value={message}>
-        <ChildComponent />
-      </MessageContext.Provider>
-    </div>
+      <h2>Notes</h2>
+      <ul>
+        {
+          notes.map(note => 
+            <Note key={note.id} note={note}/>)
+        }
+      </ul>
+      
+        <form onSubmit={addNote}>
+          <input 
+            value={newNote}
+            placeholder='add a new note...'
+            onChange={handleNoteChange}
+            
+          />
+          <button type='submit'>Save Note</button>
+        </form>
+      </div>
+    
   )
 }
 
-export {App as default, MessageContext};
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+function Note({note}) {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+export default Note;
