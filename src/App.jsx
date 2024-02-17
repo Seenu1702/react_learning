9c78c70cec960ce439808947a38e2f28774d9f71
Seenu1702 <seenusubramaniyan17@gmail.com>
2024-02-17 13:27:49 +0530

    Read method in CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,78 @@
-import React, { useReducer } from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
-const initialState = {
-  isActive: false,
-};
+function App(props) {
 
-function reducer(state, action){
-  switch (action.type) {
-    case 'toggle':
-      return {isActive: !state.isActive};
+  const [notes, setNotes] = useState([]);
+  const [showStatus, setShowStatus] = useState('all')
+
+
+  // get the data
+  useEffect(()=>{
+    // run for the first time and only once
+    setNotes(props.notes);
+  }, []);
+
+  // console.log(notes);
+
+  let filterdNotes = (notes, showStatus) => {
+    switch(showStatus){
+      case 'all':
+        return notes;
+      case 'imp':
+        return notes.filter(note => note.important === true);
+      case 'nonimp':
+        return notes.filter(note => note.important === false);
+    }
   }
-}
 
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  let notesFiltered = filterdNotes(notes, showStatus);
+
+  const handleStatusChange = (event) => {
+    // console.log(event.target.value);
+    setShowStatus(event.target.value);
+    
+  }
+  
   return (
     <div>
-      <h2>User Profile</h2>
-      <p>Account Active: {state.isActive ? 'Yes' : 'No'} </p>
-      <p>{state.isActive ? 'Your account is active' : 'Your account is inactive'}</p>
-      <button onClick={() => dispatch({type: 'toggle'})}>Toggle Profile</button>
+      <h2>Notes</h2>
+      <label className='label'>
+        <input 
+          type="radio" 
+          name='filter'
+          value='all'  
+          checked= {showStatus === 'all'}
+          onChange={handleStatusChange}/> All Notes
+      </label>
+      <label className='label'>
+        <input 
+          type="radio"
+          name='filter'
+          value='imp'
+          checked= {showStatus === 'imp'}
+          onChange={handleStatusChange} /> Important Notes
+      </label>
+      <label className='label'>
+        <input 
+          type="radio"
+          name='filter'
+          value='nonimp'
+          checked= {showStatus === 'nonimp'}
+          onChange={handleStatusChange} /> Non Important Notes
+      </label>
+      <ul>
+        {
+          notesFiltered.map(note => 
+            <li key={note.id}>{note.content}</li>
+            )
+        }
+      </ul>
     </div>
+    
   )
 }
 
